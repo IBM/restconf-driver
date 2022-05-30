aa5be7d542df61aaba59597352b00665cfa8fe10
root <root@Dev-Mahesh1.fyre.ibm.com>
2022-05-30 04:44:21 -0700

    resricting all  /management API calls excluding /info and /health

--- src/main/java/com/ibm/restconf/config/WebSecurityConfiguration.java
+++ src/main/java/com/ibm/restconf/config/WebSecurityConfiguration.java
@@ -1,16 +1,41 @@
 package com.ibm.restconf.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration("WebSecurityConfiguration")
 @EnableWebSecurity
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
- @Override
+    @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable().authorizeRequests().antMatchers("/api/**").permitAll();
+        http.csrf().disable()
+                    .authorizeRequests()
+                    .antMatchers("/management/**").hasRole("USER")
+                    .anyRequest().denyAll()
+                    .and()
+                    .httpBasic();
+    }
+
+    @Override
+    public void configure(WebSecurity web) {
+        web.ignoring().antMatchers("/api/**", "/management/health", "/management/info");
+    }
+
+    @Bean
+    @Override
+    public UserDetailsService userDetailsService() {
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+        manager.createUser(User.withDefaultPasswordEncoder().username("user").password("password").roles("USER").build());
+        manager.createUser(User.withDefaultPasswordEncoder().username("user_with_no_roles").password("password").roles("NONE").build());
+        manager.createUser(User.withDefaultPasswordEncoder().username("locked_user").password("password").roles("USER").accountLocked(true).build());
+        return manager;
     }
 }
