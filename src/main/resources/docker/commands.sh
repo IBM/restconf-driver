53f5c494ee559785fb47608f1f15edc1261c5641
rkumar8j <rkumar8j@in.ibm.com>
2022-08-02 09:20:41 +0530

    Enabled Logging changes
    Signed-off-by: rkumar8j <rkumar8j@in.ibm.com>

--- src/main/resources/docker/commands.sh
+++ src/main/resources/docker/commands.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
+mkdir -p /var/lm/truststore/
+TRUSTSTORE=/var/lm/truststore/truststore.p12
+# Add certificates to the truststore
+for cert in `find /var/lm -name *.cer -type l`; do
+  echo "$(date) Importing certificate ${cert} into ${TRUSTSTORE}"
+  $JAVA_HOME/bin/keytool -import -alias $(basename ${cert} .cer) -file ${cert} -keystore ${TRUSTSTORE} -storepass changeit -noprompt
+  TS_OPTION=-Djavax.net.ssl.trustStore=${TRUSTSTORE}
+done
+
 cd ${alm_restconf_directory}
 java $JVM_OPTIONS -jar /data/restconf-driver-@project.version@.jar
