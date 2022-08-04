dc0d4d5db12a04f4a37698bc54446561d3026ae9
Haynes Davis <Haynes.Davis@ibm.com>
2022-08-04 18:30:56 +0530

    add variables

--- src/main/resources/docker/commands.sh
+++ src/main/resources/docker/commands.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+KEYSTOREDIR=/var/lm/keystore
+KEYSTORE=$KEYSTOREDIR/keystore.p12
+CERTDIR=/var/restconfdriver/certs
+CERTKEY=$CERTDIR/tls.key
+CERT=$CERTDIR/tls.crt
+
+
 cd ${alm_restconf_directory}
-openssl pkcs12 -export -inkey /var/restconfdriver/certs/tls.key -in /var/restconfdriver/certs/tls.crt -out /var/lm/keystore/keystore.p12 -password pass:password -name "restconf-driver"
+openssl pkcs12 -export -inkey $CERTKEY -in $CERT -out $KEYSTORE -password pass:password -name "restconf-driver"
 java $JVM_OPTIONS -jar /data/restconf-driver-@project.version@.jar
