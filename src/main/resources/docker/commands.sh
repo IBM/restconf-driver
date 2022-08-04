db2cf2abd108d69f5b4c3ce7774fcbc4158d242c
Haynes Davis <Haynes.Davis@ibm.com>
2022-08-04 13:21:55 +0530

    Enable ssl

--- src/main/resources/docker/commands.sh
+++ src/main/resources/docker/commands.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 cd ${alm_restconf_directory}
+openssl pkcs12 -export -inkey /var/restconfdriver/certs/tls.key -in /var/restconfdriver/certs/tls.crt -out /var/lm/keystore/keystore.p12 -password pass:password -name "restconf-driver"
 java $JVM_OPTIONS -jar /data/restconf-driver-@project.version@.jar
